04117c054bacbb48fce163a8c19f4ed4877eacc9
Juliana Elias <eliasjuliana@gmail.com>
2023-07-24 18:21:28 -0300

    revision

--- TP 2/ejercicio11.js
+++ TP 2/ejercicio11.js
@@ -22,4 +22,5 @@ if (Math.max(edad1, edad2, edad3) === edad1) {
     console.log(`El mayor es ${nombre3}`);
 }
 
-//no incluyo los casos en que dos o las tres edades sean iguales.
+
+//no inclui los casos en que dos o las tres edades sean iguales
