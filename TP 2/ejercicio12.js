3d24deaf89879574e6995228b81b7377597ed171
Juliana Elias <eliasjuliana@gmail.com>
2023-07-24 18:30:04 -0300

    revision ejercicios 12 y 13

--- TP 2/ejercicio12.js
+++ TP 2/ejercicio12.js
@@ -1,4 +1,5 @@
 // 12- Realiza un script que genere un número aleatorio entre 1 y 99
 
+
 numAleatorio = parseInt(Math.random() *99 + 1);
 console.log(numAleatorio);
--- TP 2/ejercicio13.js
+++ TP 2/ejercicio13.js
@@ -1,6 +1,6 @@
 // 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
 
 let text = prompt('Ingrese un texto');
-
 text = text.toUpperCase();
+
 console.log (text);
