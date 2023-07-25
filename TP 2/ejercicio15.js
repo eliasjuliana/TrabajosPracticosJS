9438a51f14ac4d9d0b0b0f084066ccad36487427
eliasjuliana <122996737+eliasjuliana@users.noreply.github.com>
2023-07-25 12:24:58 -0300

    Update ejercicio15.js

--- TP 2/ejercicio15.js
+++ TP 2/ejercicio15.js
@@ -2,21 +2,22 @@
 
 let text = prompt('Ingrese un texto');
 let cantidadVocales = 0;
+let letra;
 
 for(let i = 0; i < text.length; i+=1){
     letra = text.at(i).toLowerCase();
     
 
-    if (text.at(i) === 'a' ||
-    text.at(i) === 'e' ||
-    text.at(i) === 'i' ||
-    text.at(i) === 'o' ||
-    text.at(i) === 'u' ||
-    text.at(i) === 'á' ||
-    text.at(i) === 'é' ||
-    text.at(i) === 'í' ||
-    text.at(i) === 'ó' ||
-    text.at(i) === 'ú' )
+    if (letra === 'a' ||
+    letra === 'e' ||
+    letra === 'i' ||
+    letra === 'o' ||
+    letra === 'u' ||
+    letra === 'á' ||
+    letra === 'é' ||
+    letra === 'í' ||
+    letra === 'ó' ||
+    letra === 'ú' )
     {
         cantidadVocales = cantidadVocales + 1
     } 
